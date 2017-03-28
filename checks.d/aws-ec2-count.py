3fd8be63f021193bf3a97266ff3d4f6cc091db20
Seiji Kitamura <seiji@cblt.net>
2017-03-28 22:48:55 +0900

    instance の状態を Class 化

--- checks.d/aws-ec2-count.py
+++ checks.d/aws-ec2-count.py
@@ -2,6 +2,66 @@ import boto3
 from checks import AgentCheck
 from boto3.session import Session
 
+class Instances():
+    def __init__(self):
+        self.__instances = {}
+
+    def get_availability_zones(self):
+        return self.__instances.keys()
+
+    def add_availability_zone(self, name):
+        if not self.__instances.get(name):
+            self.__instances[name] = {}
+
+    def get_instance_count(self, az, instance_type):
+        if self.__instances.get(az) and self.__instances[az].get(instance_type):
+            return self.__instances[az][instance_type]
+
+        return 0
+
+    def set_instance_count(self, az, instance_type, count):
+        self.add_availability_zone(az)
+        self.__instances[az][instance_type] = count
+        return self.__instances[az][instance_type]
+
+    def add_instance_count(self, az, instance_type, count):
+        self.add_availability_zone(az)
+
+        if not self.__instances[az].get(instance_type):
+            self.__instances[az][instance_type] = count
+        else:
+            self.__instances[az][instance_type] += count
+
+        return self.__instances[az][instance_type]
+
+    def incr_instance_count(self, az, instance_type):
+        return self.add_instance_count(az, instance_type, 1)
+
+    def has_instance_type(self, az, instance_type):
+        if self.__instances.get(az) and self.__instances[az].get(instance_type):
+            return True
+
+        return False
+
+    def get_instance_types(self, az):
+        if not self.__instances.get(az):
+            return []
+
+        return self.__instances[az].keys()
+
+    def get_instances(self):
+        instances = []
+        for az in self.get_availability_zones():
+            for instance_type in self.get_instance_types(az):
+                instance = {
+                    'availability_zone': az,
+                    'instance_type'    : instance_type,
+                    'count'            : self.get_instance_count(az, instance_type),
+                }
+                instances.append(instance)
+
+        return instances
+
 class AwsEc2Count(AgentCheck):
     def check(self, instance):
         if not instance.get('region'):
@@ -13,71 +73,44 @@ class AwsEc2Count(AgentCheck):
 
         running_instances = self.__get_running_instances(ec2)
         self.log.info('running_instances')
-        for availability_zone in running_instances.keys():
-            self.log.info(availability_zone)
-            for instance_type in running_instances[availability_zone].keys():
-                self.log.info('%s = %s' % (instance_type, running_instances[availability_zone][instance_type]))
-                self.__send_gauge(
-                    'running.count',
-                    running_instances[availability_zone][instance_type],
-                    availability_zone, instance_type
-                )
+        for instance in running_instances.get_instances():
+            az, instance_type, count = instance['availability_zone'], instance['instance_type'], instance['count']
+            self.log.info('%s : %s = %s' % (az, instance_type, count))
+            self.__send_gauge('running.count', az, instance_type, count)
 
         reserved_instances = self.__get_reserved_instances(ec2)
         self.log.info('reserved_instances')
-        for availability_zone in reserved_instances.keys():
-            self.log.info(availability_zone)
-            for instance_type in reserved_instances[availability_zone].keys():
-                self.log.info('%s = %s' % (instance_type, reserved_instances[availability_zone][instance_type]))
-                self.__send_gauge(
-                    'reserved.count',
-                    reserved_instances[availability_zone][instance_type],
-                    availability_zone, instance_type
-                )
+        for instance in reserved_instances.get_instances():
+            az, instance_type, count = instance['availability_zone'], instance['instance_type'], instance['count']
+            self.log.info('%s : %s = %s' % (az, instance_type, count))
+            self.__send_gauge('reserved.count', az, instance_type, count)
 
         ondemand_instances = self.__get_ondemand_instances(running_instances, reserved_instances)
         self.log.info('ondemand_instances')
-        for availability_zone in ondemand_instances.keys():
-            self.log.info(availability_zone)
-            for instance_type in ondemand_instances[availability_zone].keys():
-                self.log.info('%s = %s' % (instance_type, ondemand_instances[availability_zone][instance_type]))
-                if ondemand_instances[availability_zone][instance_type] >= 0:
-                    self.__send_gauge(
-                        'ondemand.count',
-                        ondemand_instances[availability_zone][instance_type],
-                        availability_zone, instance_type
-                    )
-                    if reserved_instances.get(availability_zone) and reserved_instances[availability_zone].get(instance_type):
-                        self.__send_gauge(
-                            'reserved.unused',
-                            0,
-                            availability_zone, instance_type
-                        )
-                else:
-                    self.__send_gauge(
-                        'ondemand.count',
-                        0,
-                        availability_zone, instance_type
-                    )
-                    self.__send_gauge(
-                        'reserved.unused',
-                        abs(ondemand_instances[availability_zone][instance_type]),
-                        availability_zone, instance_type
-                    )
+        for instance in ondemand_instances.get_instances():
+            az, instance_type, count = instance['availability_zone'], instance['instance_type'], instance['count']
+            self.log.info('%s : %s = %s' % (az, instance_type, count))
+            if count >= 0:
+                self.__send_gauge('ondemand.count', az, instance_type, count)
+                if reserved_instances.has_instance_type(az, instance_type):
+                    self.__send_gauge('reserved.unused', az, instance_type, 0)
+            else:
+                self.__send_gauge('ondemand.count', az, instance_type, 0)
+                self.__send_gauge('reserved.unused', az, instance_type, abs(count))
 
-    def __send_gauge(self, metric, count, availability_zone, instance_type):
+    def __send_gauge(self, metric, az, instance_type, count):
         prefix = 'aws_ec2_count.'
         self.gauge(
             prefix + metric,
             count,
             tags=[
-                'ac-availability-zone:%s' % availability_zone,
+                'ac-availability-zone:%s' % az,
                 'ac-instance-type:%s'     % instance_type
             ]
         )
 
     def __get_running_instances(self, ec2):
-        instances = {}
+        instances = Instances()
         next_token = ''
         while True:
             running_instances = ec2.describe_instances(
@@ -98,16 +131,10 @@ class AwsEc2Count(AgentCheck):
                     if running_instance.get('Platform'):
                         next
 
-                    instance_type     = running_instance['InstanceType']
-                    availability_zone = running_instance['Placement']['AvailabilityZone']
-
-                    if not instances.get(availability_zone):
-                        instances[availability_zone] = {}
-
-                    if not instances[availability_zone].get(instance_type):
-                        instances[availability_zone][instance_type] = 1
-                    else:
-                        instances[availability_zone][instance_type] += 1
+                    instances.incr_instance_count(
+                        running_instance['Placement']['AvailabilityZone'],
+                        running_instance['InstanceType'],
+                    )
 
             if running_instances.get('NextToken'):
                 next_token = running_instances['NextToken']
@@ -117,8 +144,7 @@ class AwsEc2Count(AgentCheck):
         return instances
 
     def __get_reserved_instances(self, ec2):
-        instances = {}
-        next_token = ''
+        instances = Instances()
 
         reserved_instances = ec2.describe_reserved_instances(
             Filters = [
@@ -130,37 +156,32 @@ class AwsEc2Count(AgentCheck):
         )
 
         for reserved_instance in reserved_instances['ReservedInstances']:
-            instance_type     = reserved_instance['InstanceType']
-            instance_count    = reserved_instance['InstanceCount']
-            availability_zone = reserved_instance['AvailabilityZone']
-
-            if not instances.get(availability_zone):
-                instances[availability_zone] = {}
-
-            if not instances[availability_zone].get(instance_type):
-                instances[availability_zone][instance_type] = instance_count
-            else:
-                instances[availability_zone][instance_type] += instance_count
+            instances.add_instance_count(
+                reserved_instance['AvailabilityZone'],
+                reserved_instance['InstanceType'],
+                reserved_instance['InstanceCount'],
+            )
 
         return instances
 
     def __get_ondemand_instances(self, running_instances, reserved_instances):
-        instances = {}
+        instances = Instances()
 
-        for availability_zone in running_instances.keys():
-            instances[availability_zone] = {}
-            for instance_type in running_instances[availability_zone].keys():
-                if reserved_instances.get(availability_zone) and reserved_instances[availability_zone].get(instance_type):
-                    instances[availability_zone][instance_type] = running_instances[availability_zone][instance_type] - reserved_instances[availability_zone][instance_type]
-                else:
-                    instances[availability_zone][instance_type] = running_instances[availability_zone][instance_type]
+        for az in reserved_instances.get_availability_zones():
+            for instance_type in reserved_instances.get_instance_types(az):
+                instances.set_instance_count(
+                    az,
+                    instance_type,
+                    -1 * reserved_instances.get_instance_count(az, instance_type),
+                )
 
-        for availability_zone in reserved_instances.keys():
-            if not instances.get(availability_zone):
-                instances[availability_zone] = {}
-            for instance_type in reserved_instances[availability_zone].keys():
-                if (not running_instances.get(availability_zone)) or (not running_instances[availability_zone].get(instance_type)):
-                    instances[availability_zone][instance_type] = -1 * reserved_instances[availability_zone][instance_type]
+        for az in running_instances.get_availability_zones():
+            for instance_type in running_instances.get_instance_types(az):
+                instances.add_instance_count(
+                    az,
+                    instance_type,
+                    running_instances.get_instance_count(az, instance_type),
+                )
 
         return instances
 
